79281baae53bc3e9b97572d0ca21fca8419c0a7c
Xavi-P <Xman08711@gmail.com>
2025-10-27 12:21:57 -0400

    Update Version-1.py

--- Version-1.py
+++ Version-1.py
@@ -1,1 +1,10 @@
+import random as rd
 
+magic_number= rd.randint(6,7)
+# print(magic_number)
+
+guess=int(input("what is your guess?"))
+if guess == magic_number:
+  print("your right")
+else:
+  print("your dumb")
