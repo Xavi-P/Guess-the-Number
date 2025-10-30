bc164fca08ba9a32118bc6dd75901fe5e3156c41
Xavi-P <Xman08711@gmail.com>
2025-10-30 12:53:18 -0400

    Create Version-3.py

---
+++ Version-3.py
@@ -0,0 +1,18 @@
+import random as rd
+
+magic_number= rd.randint(1,10)
+print(magic_number)
+
+guess=int(input("what is your guess?:"))
+counter=3
+
+while guess != magic_number and counter > 0 :
+  counter-=1
+
+  if counter==0:
+    print("gameover")
+  else:
+    guess=int(input("try again:"))
+  
+if guess==magic_number:
+    print("you win!")
